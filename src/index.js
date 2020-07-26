35e47cf2cae7ea00999dd5d2b2dba6e750babcad
Manan Patel <manan77patel@gmail.com>
2020-07-26 12:59:34 +0530

    working copy for binance

---
+++ src/index.js
@@ -0,0 +1,109 @@
+import Binance from 'binance-api-node'
+import { logger } from './logger'
+import axios from 'axios'
+import moment from 'moment'
+require('dotenv').config()
+import fs from 'fs'
+let dir = 'assets'
+let path = `${dir}/ binance - assets.txt`
+
+// Authenticated client, can make signed calls
+const binance = Binance({
+    apiKey: process.env.binance_API_KEY,
+    apiSecret: process.env.binance_SECRET
+})
+
+async function startLoop() {
+
+    while (true) {
+        try {
+            logger.info(`Starting next loop at ${moment(Date.now()).format('DD/MM/YYYY HH:mm:ss')} `)
+            let { balances } = await binance.accountInfo()
+            let newAssets = await Promise.all(balances.map(item => item.asset))
+            logger.info(" -----  New Assets -------")
+            logger.info(newAssets)
+            let prevAssets = getAssets(newAssets)
+            logger.info(" -----  Prev Assets -------")
+            logger.info(prevAssets)
+            if (prevAssets) {
+                let assetDiff = getAssetDiff(prevAssets, newAssets)
+                if (assetDiff.length) {
+                    await sendMessage(`
+xxx[Binance] NEW SYMBOL FOUND xxxx
+${ assetDiff}
+`)
+                } else {
+                    logger.info(`No new symbol found`)
+                }
+
+            }
+            await sleep(4)
+        } catch (err) {
+            if (!err.message.includes("failed to meet quorum")) {
+                await sendMessage(`
+xxxxx ERROR xxxxxx
+[Launchpad]
+${ err.message}
+`)
+            }
+            console.error(err)
+
+        }
+    }
+
+}
+
+function getAssets(assets) {
+
+    //check if prev asset file exists
+    if (!fs.existsSync(path)) {
+        //if not, make a new assets dir
+        fs.mkdirSync(dir)
+        // and write a new assets file
+        fs.writeFileSync(path, assets)
+        return null
+    } else {
+        //if exists, read prev assets file
+        let prevAssets = fs.readFileSync(path, 'utf8')
+        return prevAssets.split(',')
+    }
+}
+
+async function sendMessage(message) {
+    try {
+        console.log(`Sending message::
+${ message} `)
+        let response = await axios({
+            url: process.env.NIMROD_API_URL,
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({
+                "api_key": process.env.NIMROD_API_KEY,
+                "message": message
+            })
+        })
+        if (response.status == 200) {
+            return response.data
+        } else {
+            throw new Error(response.status)
+        }
+
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000))
+}
+
+function getAssetDiff(prevAssets, newAssets) {
+    return newAssets.filter(a => !prevAssets.includes(a))
+}
+
+
+startLoop()
---
+++ src/logger.js
@@ -0,0 +1,72 @@
+/* logging middleware */
+import { createLogger, format, transports } from 'winston'
+import fs from 'fs'
+import path from 'path'
+import moment from 'moment'
+require('winston-daily-rotate-file')
+const env = process.env.NODE_ENV || 'development'
+const logDir = 'logs'
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir)
+}
+
+const filename = path.join(logDir, `launchpad-${moment().utc().format('DD-MM-YYYY_HH-mm')}.log`)
+
+var transport = new transports.DailyRotateFile({
+  filename,
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '50m',
+  maxFiles: '4d'
+})
+
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} : Launchpad : ${info.level} :: ${info.message}`)
+  ),
+  // You can also comment out the line above and uncomment the line below for JSON format
+  //format: format.json(),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(info => `${info.timestamp} :: ${info.level} :: ${info.message}`)
+      )
+    }),
+    transport
+  ]
+})
+
+const getFileName = () => {
+  let fileName = ""
+  let rowNumber
+  let columnNumber
+  let currentStackPosition = 1 // this is the value representing the position of your caller in the error stack.
+  try {
+    throw new Error("Custom Error")
+  } catch (e) {
+    Error["prepareStackTrace"] = () => {
+      return arguments[1]
+    }
+    Error.prepareStackTrace(e, function () { })
+    fileName = e.stack[currentStackPosition].getFileName()
+    rowNumber = e.stack[currentStackPosition].getLineNumber()
+    columnNumber = e.stack[currentStackPosition].getColumnNumber()
+  }
+  return {
+    "file": fileName,
+    "row": rowNumber,
+    "column": columnNumber
+  }
+}
+
+export { logger }
